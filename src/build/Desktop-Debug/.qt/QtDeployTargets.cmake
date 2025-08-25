33ffab56e86e836d58577ae9ebcf662fcaae49ef
LEYLA <Leyla@tutamail.com>
2025-08-25 23:33:14 +0100

    add more placeholders

---
+++ src/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appSimpleXat_FILE /home/leyla/Projects/Git/SimpleXat/src/build/Desktop-Debug/appSimpleXat)
+set(__QT_DEPLOY_TARGET_appSimpleXat_TYPE EXECUTABLE)
---
+++ src/build/Desktop-Debug/.qt/appSimpleXat_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appSimpleXat")
+set(working_dir "/home/leyla/Projects/Git/SimpleXat/src")
+set(src_and_dest_list
+    "/home/leyla/Projects/Git/SimpleXat/src/Main.qml"
+    "/home/leyla/Projects/Git/SimpleXat/src/build/Desktop-Debug/SimpleXat/Main.qml"
+
+)
+set(timestamp_file "/home/leyla/Projects/Git/SimpleXat/src/build/Desktop-Debug/.qt/appSimpleXat_qml.txt")
