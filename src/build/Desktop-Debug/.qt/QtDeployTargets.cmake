87696dc605406f04e4f265c9767261f4f6132605
LEYLA <Leyla@tutamail.com>
2025-08-25 23:51:00 +0100

    clean up debug dir

--- src/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appSimpleXat_FILE /home/leyla/Projects/Git/SimpleXat/src/build/Desktop-Debug/appSimpleXat)
-set(__QT_DEPLOY_TARGET_appSimpleXat_TYPE EXECUTABLE)
--- src/build/Desktop-Debug/.qt/appSimpleXat_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appSimpleXat")
-set(working_dir "/home/leyla/Projects/Git/SimpleXat/src")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/leyla/Projects/Git/SimpleXat/src/build/Desktop-Debug/.qt/appSimpleXat_res.txt")
